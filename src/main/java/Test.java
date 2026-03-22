6f17ee699cba37232a4412ce93d32edcb0522a35
Swara Shivankar <swarashivankar2508@gmail.com>
2026-03-22 17:03:40 +0530

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,15 +1,25 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Test;
 
 public class Test {
-    public static void main(String[] args) {
+
+    @Test
+    public void openGoogle() {
+
+        // ChromeDriver path
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 
+        // Launch browser
         WebDriver driver = new ChromeDriver();
+
+        // Open Google
         driver.get("https://www.google.com");
 
+        // Print message
         System.out.println("Opened Google successfully");
 
+        // Close browser
         driver.quit();
     }
 }
